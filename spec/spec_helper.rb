d8aa9d0749cfa0b398982144b57fe43c0502241a
Alex Leschenko <leschenko.al@gmail.com>
2013-06-21 00:17:25 +0300

    move elasticsearch.log from tmp for Travis CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'active_support/core_ext'
 require 'elastic_ransack'
 
 Tire.configure do
-  logger 'tmp/elasticsearch.log'  # Commented out logger line here so that it doesn't break specs when tmp directory doesn't exist.
+  logger 'elasticsearch.log'
   url 'http://localhost:9200'
   pretty 1
 end
